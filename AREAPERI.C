793b9a80ee908dad2ba44cf4bfdce567e559dd4e
Vijaycv0 <cvvijay652@gmail.com>
2025-09-19 10:12:55 +0530

    Add files via upload

---
+++ AREAPERI.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int l,w,a,p;
+clrscr();
+printf("enter the rectangle lenth:");
+scanf("%d",&l);
+printf("enter the rectangle width:");
+scanf("%d",&w);
+a=l*w;
+p=(2*l)+(2*w);
+printf("rectangle area=""%d",a);
+printf("\nrectangle perimeter=""%d",p);
+getch();
+}
+
+
+
+
+
+
+
+
+
