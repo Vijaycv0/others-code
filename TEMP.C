20ad3b842d178e86a718e349cfe0354470e2ca92
Vijaycv0 <cvvijay652@gmail.com>
2025-09-19 10:30:21 +0530

    Add files via upload

---
+++ TEMP.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int c,f;
+clrscr();
+printf("enter there corrent temperature in celsius:");
+scanf("%d",&c);
+f=(c*9/5)+32;
+printf("celsius to fahrenheit=""%d",f);
+getch();
+}
+
+
+
+
+
+
+
+
+
