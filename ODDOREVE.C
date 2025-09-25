02fe8460f1bcb0aaddcbdf798a7786e939054b65
Vijaycv0 <cvvijay652@gmail.com>
2025-09-25 14:00:31 +0530

    Add files via upload

---
+++ ODDOREVE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter a number:");
+scanf("%d",&a);
+if(a%2=0);
+{
+printf("the number is even");
+}else
+{
+printf("th number is odd");
+}
+getch();
+}
