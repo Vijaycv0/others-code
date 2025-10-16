36e87cb190390a0050bd9fdffbd4aefcc6211226
Vijaycv0 <cvvijay652@gmail.com>
2025-10-16 13:46:58 +0530

    Add files via upload

---
+++ POWER.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int s,a,t,n=1;
+clrscr();
+printf("enter a number=");
+scanf("%d",&a);
+printf("enter the power of the number=");
+scanf("%d",&t);
+while(n==t)
+{
+s=a*a;
+}
+printf("%d",s);
+getch();
+}
