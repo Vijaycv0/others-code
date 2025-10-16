3ae02681ecbf79d0c236e54776af20e8d060c4b8
Vijaycv0 <cvvijay652@gmail.com>
2025-10-16 13:51:27 +0530

    Update POWER.C

--- POWER.C
+++ POWER.C
@@ -1,16 +1,17 @@
 #include<stdio.h>
 void main()
 {
-int s,a,t,n=1;
+int s=1,a,t,n;
 clrscr();
 printf("enter a number=");
 scanf("%d",&a);
 printf("enter the power of the number=");
 scanf("%d",&t);
-while(n==t)
+for(n=1;n<=t;n++)
 {
-s=a*a;
+s=s*a;
 }
 printf("%d",s);
 getch();
 }
+
